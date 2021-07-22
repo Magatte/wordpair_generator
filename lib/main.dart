451fa6789c46c1f60ce42c7fe9c9737d17065236
Magatte <magattestg@gmail.com>
2021-07-22 18:50:57 +0200

    (+) Added WordPairs list

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-    )
   }
 }
